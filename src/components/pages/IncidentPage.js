8016b8ad1682b690c6bda55499c83a4777fd7887
noahhhughes <noah.hughes357@gmail.com>
2019-06-02 19:24:21 -0400

    Incident Page

---
+++ src/components/pages/IncidentPage.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import {AppRegistry, TouchableOpacity, Flatlist, Text, View, Image, Alert, stylestyleSheet} from 'react-native';
+
+export default class IncidentPage extends React.PureComponent{
+    constructor(){
+
+    }
+    render() {
+        return (
+            <View style={styles.incidentPageLayout}>
+                <View style={styles.navBar}>
+
+                </View>
+                <View style={styles.pageLayout}>
+                    <View style={styles.stagingArea}>
+                        <View style={styles.stagingList}>
+
+                        </View>
+                        <View style={styles.personnelList}>
+
+                        </View>
+                    </View>
+                    <View style={styles.groupArea}>
+                        <View style={styles.subGroupArea}>
+                            <View style={styles.group}>
+
+                            </View>
+                            <View style={styles.group}>
+                                
+                            </View>
+                        </View>
+                        <View style={styles.subGroupArea}>
+                            <View style={styles.group}>
+
+                            </View>
+                            <View style={styles.group}>
+                                
+                            </View>
+                        </View>
+                        <View style={styles.subGroupArea}>
+                            <View style={styles.group}>
+
+                            </View>
+                            <View style={styles.group}>
+                                
+                            </View>
+                        </View>
+                    </View>
+                </View>
+            </View>
+        )
+    }
+}

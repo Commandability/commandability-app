6926e91f114e242942a7d4822a1a8243540a7ea5
noahhhughes <noah.hughes357@gmail.com>
2019-06-04 19:14:45 -0400

    Router.js instructions

--- src/Router.js
+++ src/Router.js
@@ -9,14 +9,32 @@ import ReportPage from './components/pages/ReportPage';
 const RouterComponent = () => {
     return (
         <Router>
-            <Scene key='root'>
-                <Scene key='login' component={LoginPage} title='Please Login' initial ></Scene>
-                <Scene key='main' component={MainPage} title='Main Page'></Scene>
-                <Scene key='incident' component={IncidentPage} title='Incident Page'></Scene>
-                <Scene key='report' component={ReportPage} title='Report Page'></Scene>
+            <Scene key='root' hideNavBar>
+                <Scene key='auth' hideNavBar>
+                    <Scene key='login' component={LoginPage} title='Please Login' initial ></Scene>
+                </Scene>
+                <Scene key='home' hideNavBar>
+                    <Scene key='main' component={MainPage} title='Main Page' hideNavBar></Scene>
+                    <Scene key='report' component={ReportPage} title='Report Page'></Scene>
+                </Scene>
+                <Scene key='management' hideNavBar>
+                    <Scene key='incident' component={IncidentPage} title='Incident Page' hideNavBar></Scene>
+                    <Scene key='editPerson' title='Please enter the new personnel informaiton'></Scene>
+                    <Scene key='editGroup' title='Please enter the new group information'></Scene>
+                </Scene>
             </Scene>
         </Router>
     );
 }
 
 export default RouterComponent;
+
+// Add this to the action/index.js to properly link paths:
+//
+// import { Actions } from 'react-native-router-flux';
+//
+// For each action that results in a screen change or new page, add in Actions.'key'(), where
+// the key value matches the key to the page you want to switch to. This is added just after 
+// the dispatch(); in the appropriate action. For nested scenes, do the upper level one. 
+//
+// Actions.home()

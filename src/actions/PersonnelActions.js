0c78ca02b24508c6e88a32ca8283c9e93d87f5ed
dleemorrow <d.lee.morrow@gmail.com>
2019-08-07 13:51:14 -0400

    fix location on addPersonnel

--- src/actions/PersonnelActions.js
+++ src/actions/PersonnelActions.js
@@ -1,16 +1,17 @@
 import { Actions } from "react-native-router-flux";
 import uuidv4 from "uuid/v4";
 import { ADD_PERSONNEL, REMOVE_PERSONNEL, SET_LOCATION, SELECT_PERSON, DESELECT_PERSON, CLEAR_SELECTED_PERSONNEL } from "./types";
+import { ROSTER } from "../reducers/locations";
 
 export const addPersonnel = ({
   badge,
   firstName,
   lastName,
-  location,
   rank,
   shift
 }) => {
   const id = uuidv4();
+  const location = ROSTER;
   return {
     type: ADD_PERSONNEL,
     payload: { id, badge, firstName, lastName, location, rank, shift }

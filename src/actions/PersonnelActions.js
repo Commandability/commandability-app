e9a2dede1355016259f70250f5afb7e8a7854a65
dleemorrow <d.lee.morrow@gmail.com>
2019-08-07 13:41:21 -0400

    add selected actions

--- src/actions/PersonnelActions.js
+++ src/actions/PersonnelActions.js
@@ -1,8 +1,15 @@
 import { Actions } from "react-native-router-flux";
 import uuidv4 from "uuid/v4";
-import { ADD_PERSONNEL, REMOVE_PERSONNEL, SET_LOCATION } from "./types";
+import { ADD_PERSONNEL, REMOVE_PERSONNEL, SET_LOCATION, SELECT_PERSON, DESELECT_PERSON, CLEAR_SELECTED_PERSONNEL } from "./types";
 
-export const addPersonnel = ({ badge, firstName, lastName, location, rank, shift }) => {
+export const addPersonnel = ({
+  badge,
+  firstName,
+  lastName,
+  location,
+  rank,
+  shift
+}) => {
   const id = uuidv4();
   return {
     type: ADD_PERSONNEL,
@@ -18,4 +25,18 @@ export const removePersonnel = ({ id }) => ({
 export const setLocation = ({ id }, location) => ({
   type: SET_LOCATION,
   payload: { id, location }
+});
+
+export const selectPerson = ({ id }) => ({
+  type: SELECT_PERSON,
+  payload: { id }
+});
+
+export const deselectPerson = ({ id }) => ({
+  type: DESELECT_PERSON,
+  payload: { id }
+});
+
+export const clearSelectedPersonnel = () => ({
+  type: CLEAR_SELECTED_PERSONNEL
 });

1412e09d79a01b1636ba05c475b2c46fbc439b92
noahhhughes <noah.hughes357@gmail.com>
2019-08-20 05:58:43 -0400

    Added in default pages

---
+++ src/components/pages/ReportPage.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {AppRegistry, TouchableOpacity, Flatlist, Text, View, Image, Alert, stylestyleSheet} from 'react-native';
+
+export default class ReportPage extends React.PureComponent{
+    render() {
+        return(
+            <View>
+                <Text> Report Page </Text>
+            </View>
+        )
+    }
+}

df543a1ff9aaf0c74d1cae526271503f1cce8755
dleemorrow <d.lee.morrow@gmail.com>
2019-09-15 17:24:02 -0400

    fix file imports

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,15 +1,11 @@
 import GroupPrompt from "./GroupPrompt";
 import IncidentScreen from "./IncidentScreen";
-import LoginPage from "./LoginPage";
-import MainPage from "./MainPage";
 import PersonPrompt from "./PersonPrompt";
 import ReportPage from "./ReportPage";
 
 export {
   GroupPrompt,
   IncidentScreen,
-  LoginPage,
-  MainPage,
   PersonPrompt,
   ReportPage,
 };

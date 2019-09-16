bc0a0966457fb0313c3f1c606092d165bcb61bb4
dleemorrow <d.lee.morrow@gmail.com>
2019-09-16 17:40:06 -0400

    add call to selector in reportManager

--- src/modules/reportManager.js
+++ src/modules/reportManager.js
@@ -6,12 +6,13 @@
 import AsyncStorage from "@react-native-community/async-storage";
 
 import { store } from "../App.js";
+import { getLog } from "../reducers";
 
 // log state should include time stamps for each action
 // create start report action that logs the report start time. This wil be used in the file name.
 export const loadLatestReport = () => {
   const fileString = "";
-  store.getState().log.forEach(() => {}); // append each action in log to filestring
+  // getLog(store.getState()).forEach(() => {}); // append each action in log to filestring
 };
 
 export const saveLatestReport = () => {};

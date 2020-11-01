ef31a4a8d4560e052814b9c02013d477cf59fd6b
Noah Hughes <noah.hughes357@gmail.com>
2020-11-01 16:26:02 -0500

    removed unnessary roster code

--- src/components/roster/index.js
+++ src/components/roster/index.js
@@ -5,22 +5,12 @@
  */
 
 import React, { Component } from 'react';
-import { Alert, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Text, TextInput, View } from 'react-native';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import RosterList from '../roster-list';
-import {
-  getSelectedLocationId,
-  getSelectedPersonnel,
-  getTheme,
-} from '../../redux/selectors';
-import {
-  clearSelectedPersonnel,
-  setPersonLocationId,
-  removePerson,
-} from '../../redux/actions';
-import { STAGING, ROSTER } from '../../modules/location-ids';
+import { getTheme } from '../../redux/selectors';
 import themeSelector from '../../modules/themes';
 import createStyleSheet from './styles';
 
@@ -33,22 +23,13 @@ class Roster extends Component {
   }
 
   render() {
-    const { selectedLocationId, theme } = this.props;
-
-    const renderOverlay =
-      selectedLocationId && selectedLocationId !== ROSTER ? true : false;
+    const { theme } = this.props;
 
     const colors = themeSelector(theme);
     const styles = createStyleSheet(colors);
 
     return (
       <View style={styles.container}>
-        {renderOverlay && (
-          <TouchableOpacity
-            style={styles.overlay}
-            onPress={this._onRosterPressed}
-          />
-        )}
         <View style={styles.header}>
           <Text style={styles.headerContent}> Roster </Text>
         </View>
@@ -68,26 +49,14 @@ class Roster extends Component {
 
 // props validation
 Roster.propTypes = {
-  selectedLocationId: PropTypes.string,
-  locationId: PropTypes.string,
-  selectedPersonnel: PropTypes.array,
-  clearSelectedPersonnel: PropTypes.func,
-  setPersonLocationId: PropTypes.func,
-  removePerson: PropTypes.func,
   theme: PropTypes.string,
 };
 
 const mapStateToProps = state => ({
-  selectedLocationId: getSelectedLocationId(state),
-  selectedPersonnel: getSelectedPersonnel(state),
   theme: getTheme(state),
 });
 
 export default connect(
   mapStateToProps,
-  {
-    clearSelectedPersonnel,
-    removePerson,
-    setPersonLocationId,
-  }
+  null
 )(Roster);
--- src/components/roster/styles.js
+++ src/components/roster/styles.js
@@ -2,16 +2,6 @@ import { StyleSheet } from 'react-native';
 
 export default colors =>
   StyleSheet.create({
-    overlay: {
-      backgroundColor: colors.overlay,
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      zIndex: 1,
-      width: '100%',
-      height: '100%',
-      opacity: 0.2,
-    },
     queryInput: {
       height: 40,
       color: colors.text.main,

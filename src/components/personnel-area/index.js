a84d7f8ecc7b7d60229bd3f12b65403d46fb9b22
Noah Hughes <noah.hughes357@gmail.com>
2020-12-01 19:01:12 -0500

    fixed navigation crash

--- src/components/personnel-area/index.js
+++ src/components/personnel-area/index.js
@@ -5,65 +5,42 @@
  *
  */
 
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import { TouchableOpacity, Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { useNavigation } from '@react-navigation/native';
-import PropTypes from 'prop-types';
 
 import { getTheme } from '../../redux/selectors';
 import themeSelector from '../../modules/themes';
 import createStyleSheet from './styles';
 import { Staging, RemovePersonnel } from '..';
 
-class PersonnelArea extends Component {
-  _onAddPersonnelPressed = () => {
-    const {
-      navigation: { navigate },
-    } = this.props;
+const PersonnelArea = () => {
+  const navigation = useNavigation();
+  const theme = useSelector(state => getTheme(state));
+
+  const onAddPersonnelPressed = () => {
+    const { navigate } = navigation;
     navigate('PersonnelPrompt');
   };
 
-  render() {
-    const { theme } = this.props;
-    const colors = themeSelector(theme);
-    const styles = createStyleSheet(colors);
+  const colors = themeSelector(theme);
+  const styles = createStyleSheet(colors);
 
-    return (
-      <View style={styles.container}>
-        <TouchableOpacity
-          style={styles.option}
-          onPress={this._onAddPersonnelPressed}
-        >
-          <Text style={styles.optionContent}>ADD PERSONNEL</Text>
-          <Icon name="arrow-right" style={styles.optionContent} />
-        </TouchableOpacity>
-        <Staging />
-        <RemovePersonnel />
-      </View>
-    );
-  }
-}
-
-// props validation
-PersonnelArea.propTypes = {
-  navigation: PropTypes.object,
-  theme: PropTypes.string,
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.option}
+        onPress={onAddPersonnelPressed}
+      >
+        <Text style={styles.optionContent}>ADD PERSONNEL</Text>
+        <Icon name="arrow-right" style={styles.optionContent} />
+      </TouchableOpacity>
+      <Staging />
+      <RemovePersonnel />
+    </View>
+  );
 };
 
-const mapStateToProps = state => ({
-  theme: getTheme(state),
-});
-
-const ConnectWrapper = connect(
-  mapStateToProps,
-  null
-)(PersonnelArea);
-
-// Wrap and export
-export default function NavigationWrapper(props) {
-  const navigation = useNavigation();
-
-  return <ConnectWrapper {...props} navigation={navigation} />;
-}
+export default PersonnelArea;

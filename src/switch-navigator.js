377a3bdb60a69671d856766e177e45a8206530ed
Lee Morrow <leemorrow.dev@gmail.com>
2021-01-21 16:51:38 -0500

    White flash fix

--- src/switch-navigator.js
+++ src/switch-navigator.js
@@ -6,6 +6,7 @@
  */
 
 import React, { useEffect } from 'react';
+import { View, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -27,7 +28,8 @@ import {
   INCIDENT_STACK,
   END_STACK,
 } from './modules/navigation-stacks';
-import { selectStack } from './redux/selectors';
+import { selectStack, selectTheme } from './redux/selectors';
+import themeSelector from './modules/themes';
 
 const Auth = createStackNavigator();
 const Home = createStackNavigator();
@@ -38,64 +40,80 @@ const screenOptions = {
   headerShown: false,
 };
 
+const setStack = stack => {
+  switch (stack) {
+    case HOME_STACK:
+      return (
+        <Home.Navigator screenOptions={screenOptions}>
+          <Home.Screen name="HomeScreen" component={HomeScreen} />
+        </Home.Navigator>
+      );
+    case INCIDENT_STACK:
+      return (
+        <Incident.Navigator screenOptions={screenOptions}>
+          <Incident.Screen name="IncidentScreen" component={IncidentScreen} />
+          <Incident.Screen
+            name="EditGroupPrompt"
+            component={EditGroupPrompt}
+          />
+          <Incident.Screen
+            name="PersonnelPrompt"
+            component={PersonnelPrompt}
+          />
+          <Incident.Screen
+            name="AddPersonPrompt"
+            component={AddPersonPrompt}
+          />
+        </Incident.Navigator>
+      );
+    case END_STACK:
+      return (
+        <End.Navigator screenOptions={screenOptions}>
+          <End.Screen name="EndScreen" component={EndScreen} />
+          <End.Screen
+            name="ExitIncidentPrompt"
+            component={ExitIncidentPrompt}
+          />
+        </End.Navigator>
+      );
+    case AUTH_STACK:
+    default:
+      return (
+        <Auth.Navigator screenOptions={screenOptions}>
+          <Auth.Screen
+            name="AuthScreen"
+            component={AuthScreen}
+            options={{ animationTypeForReplace: 'pop' }}
+          />
+        </Auth.Navigator>
+      );
+  }
+};
+
+const createStyleSheet = colors => StyleSheet.create({
+  // eslint-disable-next-line react-native/no-unused-styles
+  container: { 
+    flex: 1,
+    backgroundColor: colors.background.one
+  }
+});
+
 const SwitchNavigator = () => {
+  const theme = useSelector(state => selectTheme(state));
   const stack = useSelector(state => selectStack(state));
 
   useEffect(() => {
     SplashScreen.hide();
   }, []);
 
-  const setStack = stack => {
-    switch (stack) {
-      case HOME_STACK:
-        return (
-          <Home.Navigator screenOptions={screenOptions}>
-            <Home.Screen name="HomeScreen" component={HomeScreen} />
-          </Home.Navigator>
-        );
-      case INCIDENT_STACK:
-        return (
-          <Incident.Navigator screenOptions={screenOptions}>
-            <Incident.Screen name="IncidentScreen" component={IncidentScreen} />
-            <Incident.Screen
-              name="EditGroupPrompt"
-              component={EditGroupPrompt}
-            />
-            <Incident.Screen
-              name="PersonnelPrompt"
-              component={PersonnelPrompt}
-            />
-            <Incident.Screen
-              name="AddPersonPrompt"
-              component={AddPersonPrompt}
-            />
-          </Incident.Navigator>
-        );
-      case END_STACK:
-        return (
-          <End.Navigator screenOptions={screenOptions}>
-            <End.Screen name="EndScreen" component={EndScreen} />
-            <End.Screen
-              name="ExitIncidentPrompt"
-              component={ExitIncidentPrompt}
-            />
-          </End.Navigator>
-        );
-      case AUTH_STACK:
-      default:
-        return (
-          <Auth.Navigator screenOptions={screenOptions}>
-            <Auth.Screen
-              name="AuthScreen"
-              component={AuthScreen}
-              options={{ animationTypeForReplace: 'pop' }}
-            />
-          </Auth.Navigator>
-        );
-    }
-  };
+  const colors = themeSelector(theme);
+  const styles = createStyleSheet(colors);
 
-  return <NavigationContainer>{setStack(stack)}</NavigationContainer>;
+  return (
+    <View style={styles.container}>
+      <NavigationContainer>{setStack(stack)}</NavigationContainer>
+    </View>
+  );
 };
 
 export default SwitchNavigator;

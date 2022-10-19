116dfdd5c1e43300f7e95c7269159b93744f4dc5
Lee Morrow <leemorrow.dev@gmail.com>
2022-10-19 17:44:20 -0400

    Update to react native 0.67 and update app icons

--- android/app/src/main/java/com/commandability/MainActivity.java
+++ android/app/src/main/java/com/commandability/MainActivity.java
@@ -1,19 +1,12 @@
 package com.commandability;
 
+import com.facebook.react.ReactActivity;
+
 // commandability-mod: react-native-bootsplash
 import android.os.Bundle;
-
-import com.facebook.react.ReactActivity;
-// commandability-mod: react-native-bootsplash
 import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
-  // commandability-mod: react-native-bootsplash
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    RNBootSplash.init(R.drawable.bootsplash, MainActivity.this); // <- display the generated bootsplash.xml drawable over our MainActivity
-  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -22,5 +15,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "commandability";
+  }
+
+  // commandability-mod: react-native-bootsplash
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    RNBootSplash.init(this); // <- initialize the splash screen
+    super.onCreate(null); // or super.onCreate(null) with react-native-screens
   }
 }
--- ios/commandability/AppDelegate.m
+++ ios/commandability/AppDelegate.m
@@ -1,11 +1,8 @@
-#import <Firebase.h> // commandability-mod: firebase
 #import "AppDelegate.h"
 
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-
-#import "RNBootSplash.h" // commandability-mod: react-native-bootsplash
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
@@ -30,10 +27,6 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-// commandability-mod: firebase
-if ([FIRApp defaultApp] == nil) {
-    [FIRApp configure];
-}
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
@@ -43,16 +36,17 @@ if ([FIRApp defaultApp] == nil) {
                                                    moduleName:@"commandability"
                                             initialProperties:nil];
 
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  if (@available(iOS 13.0, *)) {
+      rootView.backgroundColor = [UIColor systemBackgroundColor];
+  } else {
+      rootView.backgroundColor = [UIColor whiteColor];
+  }
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
-
-  [RNBootSplash initWithStoryboard:@"BootSplash" rootView:rootView]; // commandAbility-mod: react-native-bootsplash
-
   return YES;
 }
 
